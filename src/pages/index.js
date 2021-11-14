caff2624b34cfbe8f7abf4e298c6010e31a6be07
yk-doi <yk-doi@kddi.com>
2021-11-14 13:29:04 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,127 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  // const posts = data.allMarkdownRemark.nodes
+  const posts = data.allContentfulPost.edges
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Seo title="All posts" />
+        <Bio />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+      <Bio />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          // const title = post.frontmatter.title || post.fields.slug
+          const title = post.node.title || post.node.fields.slug
+
+          return (
+            // <li key={post.fields.slug}>
+            <li key={post.node.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    {/* <Link to={post.fields.slug} itemProp="url"> */}
+                    <Link to={post.node.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  {/* <small>{post.frontmatter.date}</small> */}
+                  <small>{post.node.updatedAt}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      // __html: post.frontmatter.description || post.excerpt,
+                      __html: post.node.description ? post.node.description.description : post.node.body.childMarkdownRemark.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+// export const pageQuery = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+//       nodes {
+//         excerpt
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           date(formatString: "YYYY-MM-DD")
+//           title
+//           description
+//         }
+//       }
+//     }
+//   }
+// `
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulPost {
+      edges {
+        node {
+          title
+          image {
+            file {
+              url
+            }
+          }
+          updatedAt(locale: "ja-JP", formatString: "YYYY-MM-DD")
+          description {
+            description
+          }
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/blog-post.js
@@ -0,0 +1,156 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogPostTemplate = ({ data, location }) => {
+  // const post = data.markdownRemark
+  const post = data.contentfulPost
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const { previous, next } = data
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo
+        // title={post.frontmatter.title}
+        // description={post.frontmatter.description || post.excerpt}
+        title={post.title}
+        // description={post.description.description}
+        description={post.description ? post.description.description : post.body.childMarkdownRemark.excerpt}
+      />
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          {/* <h1 itemProp="headline">{post.frontmatter.title}</h1> */}
+          {/* <p>{post.frontmatter.date}</p> */}
+          <h1 itemProp="headline">{post.title}</h1>
+          <p>{post.createdAt}</p>
+        </header>
+        <section
+          // dangerouslySetInnerHTML={{ __html: post.html }}
+          dangerouslySetInnerHTML={{ __html: post.body.childMarkdownRemark.html }}
+          itemProp="articleBody"
+        />
+        <hr />
+        <footer>
+          <Bio />
+        </footer>
+      </article>
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              // <Link to={previous.fields.slug} rel="prev">
+                // ← {previous.frontmatter.title}
+              <Link to={`/${previous.slug}`} rel="prev">
+                ← {previous.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              // <Link to={next.fields.slug} rel="next">
+              //   {next.frontmatter.title} →
+              <Link to={`/${next.slug}`} rel="next">
+                {next.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
+
+export default BlogPostTemplate
+
+// export const pageQuery = graphql`
+//   query BlogPostBySlug(
+//     $id: String!
+//     $previousPostId: String
+//     $nextPostId: String
+//   ) {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     markdownRemark(id: { eq: $id }) {
+//       id
+//       excerpt(pruneLength: 160)
+//       html
+//       frontmatter {
+//         title
+//         date(formatString: "YYYY-MM-DD")
+//         description
+//       }
+//     }
+//     previous: markdownRemark(id: { eq: $previousPostId }) {
+//       fields {
+//         slug
+//       }
+//       frontmatter {
+//         title
+//       }
+//     }
+//     next: markdownRemark(id: { eq: $nextPostId }) {
+//       fields {
+//         slug
+//       }
+//       frontmatter {
+//         title
+//       }
+//     }
+//   }
+// `
+
+export const pageQuery = graphql`
+  query BlogPostBySlug(
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulPost(id: { eq: $id }) {
+      id
+      body {
+        body
+        childMarkdownRemark {
+          html
+          excerpt
+        }
+      }
+      title
+      createdAt(locale: "ja-JP", formatString: "YYYY-MM-DD")
+      description {
+        description
+      }
+    }
+    previous: contentfulPost(id: { eq: $previousPostId }) {
+      slug
+      title
+    }
+    next: contentfulPost(id: { eq: $nextPostId }) {
+      slug
+      title
+    }
+  }
+`
+
