33af120cf67a2eb49664f3db20b6216430ca3f96
yk-doi <yk-doi@kddi.com>
2021-11-15 00:48:17 +0900

    icon

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-penguin.jpg"
+        src="../images/profile-nasu.jpg"
         width={50}
         height={50}
         quality={95}
